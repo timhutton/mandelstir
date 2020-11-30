aae5a8ac99b35b21e4852f82d4e270fe18f408e1
Tim Hutton <tihutt@microsoft.com>
2020-11-30 20:08:53 +0000

    Rewrote to store the points being tracked.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*  Copyright 2012 Tim Hutton
+/*  Copyright 2020 Tim Hutton
 
     This file is part of MandelStir.
 
@@ -15,289 +15,158 @@
     You should have received a copy of the GNU General Public License
     along with MandelStir. If not, see <http://www.gnu.org/licenses/>.         */
 
-// http://code.google.com/p/mandelstir
-// Contact: tim.hutton@gmail.com
-// http://www.sq3.org.uk
-
-// stdlib:
-#include <math.h>
-
-// STL:
-#include <iostream>
-#include <sstream>
+// stdlib
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 using namespace std;
 
-// ---------------------------------------------------------------------------------
+struct Point {
+    double x, y;
+};
 
-// return the counter-clockwise angle [0,2pi]
-double atanCCW(double y,double x)
-{
-    double a = atan2(y,x);
-    if(a<0) a += 2.0 * 3.14159265358979;
-    return a;
-}
+struct MandelbrotPoint {
+    Point c;
+    Point z;
+    Point prev_z;
 
-// ---------------------------------------------------------------------------------
-
-// raise a complex number x+yi to the p'th power (where p is a real number) in-place
-void ComplexPower(double &x,double &y,double p)
-{
-    // http://mathworld.wolfram.com/ComplexNumber.html
-    double r2 = x*x + y*y;
-    double rp = pow(r2,p/2.0);
-    double theta = atanCCW(y,x);
-    x = rp * cos(p*theta);
-    y = rp * sin(p*theta);
-}
-
-// ---------------------------------------------------------------------------------
-
-// draw a number on the image
-void DrawNumber(unsigned char* image,int W,int H,double f);
-
-// ---------------------------------------------------------------------------------
-
-int main()
-{
-    const double aspect = 4.0/3.0;
-    const double scale = 1.5;
-    const int WIDTH = int(512*aspect*scale);
-    const int HEIGHT = int(512*scale);
-    unsigned char * image = new unsigned char[HEIGHT*WIDTH*4]; // row,column BGRA
-    float * float_image = new float[HEIGHT*WIDTH*3]; // row,column RGB
-    for(int i=0;i<HEIGHT*WIDTH;i++)
-        image[i*4+3] = 255; // don't want transparency
-    
-    double xmin,xmax,ymin,ymax; // area to explore
-
-    int max_its = 100;
-
-    enum { Mandelbrot, Julia }                      algorithm =     Mandelbrot;
-    enum { All, CircleRadius2, Checkerboard, Set }  source_points = Set;
-    enum { Stirring, Linear }                       interpolation = Linear;
-
-    const double sub = 0.1; // subsample the pixels for better resolution
-
-    double speed;
-
-    int max_frames;
-
-    double zx,zy,cx,cy;
-    double tempx,t;
-    int r,g,b;
-
-    if(algorithm==Mandelbrot)
+    void update()
     {
-       xmin=-2;
-       xmax=2;
-       ymin=-1.5;
-       ymax=1.5;
-       speed = 0.005;
-       max_frames = 4000; // or as high as you like
-    }
-    else
-    {
-        cx=-0.11; cy=0.6557;   // spirally blob
-        //cx=-0.8;  cy=0.15;   // whiskery dragon
-        //cx = -0.743643887037151l; cy = 0.131825904205330; // separated whorls
-        //cx = 0.0; cy = -0.636; // romanesco broccoli
-        xmin=-1.8;
-        xmax=1.8;
-        ymin=-1.5;
-        ymax=1.5;
-        speed = 0.005;
-        max_frames = int(3.0 / speed); // just run a few times
+        prev_z.x = z.x;
+        prev_z.y = z.y;
+        z.x = prev_z.x * prev_z.x - prev_z.y * prev_z.y + c.x;
+        z.y = 2 * prev_z.x * prev_z.y + c.y;
     }
 
-    for(int frame=0;frame<max_frames;frame++)
+    Point getIntermediate(double u) const
     {
-        cout << "Frame " << frame+1 << " of " << max_frames << "\n";
-
-        double target = frame * speed;
-
-        // plot the static background image
-        for(int y=0;y<HEIGHT;y++)
-        {
-            for(int x=0;x<WIDTH;x++)
-            {
-                zx = xmin + (xmax-xmin)*(x/double(WIDTH));
-                zy = ymin + (ymax-ymin)*(y/double(HEIGHT));
-                if(algorithm == Mandelbrot)
-                {
-                    cx = zx;
-                    cy = zy;
-                }
-                r = 0; g = 0; b = 128; // dark blue inside
-                for(int it=0;it<max_its;it++)
-                {
-                    tempx = zx*zx - zy*zy + cx;
-                    zy = 2.0*zx*zy + cy;
-                    zx = tempx;
-                    if(zx*zx+zy*zy>4.0)
-                    {
-                        r = g = b = 0; // black outside
-                        break;
-                    }
-                }
-                image[y*WIDTH*4+x*4+0] = b; 
-                image[y*WIDTH*4+x*4+1] = g;
-                image[y*WIDTH*4+x*4+2] = r;
-                float_image[y*WIDTH*3+x*3+0] = float_image[y*WIDTH*3+x*3+1] = float_image[y*WIDTH*3+x*3+2] = 0.0f;
-            }
-        }
-        // show the iteration count (a floating point number!)
-        DrawNumber(image,WIDTH,HEIGHT,target);
-        int itarget = int(floor(target));
-        t = target - itarget;
-        // plot the moving points
-        for( double subx = xmin; subx < xmax; subx += sub*(xmax-xmin)/double(WIDTH) )
-        {
-            for(double suby = ymin; suby < ymax; suby += sub*(ymax-ymin)/double(HEIGHT) )
-            {
-                switch(source_points)
-                {
-                    case All: default:
-                        break;
-                    case CircleRadius2:
-                        if(subx*subx+suby*suby>4.0) continue;
-                        break;
-                    case Checkerboard:
-                        {
-                            if( ((int(WIDTH*(subx-xmin)/(xmax-xmin))/30)%2) ^ ((int(HEIGHT*(suby-ymin)/(ymax-ymin))/30)%2) )
-                                continue;
-                        }
-                        break;
-                    case Set:
-                        {
-                            // does this pixel escape?
-                            zx = subx;
-                            zy = suby;
-                            if(algorithm == Mandelbrot)
-                            {
-                                cx = zx;
-                                cy = zy;
-                            }
-                            bool escaped = false;
-                            for(int it=0;it<max_its;it++)
-                            {
-                                tempx = zx*zx - zy*zy + cx;
-                                zy = 2.0*zx*zy + cy;
-                                zx = tempx;
-                                if(zx*zx+zy*zy>4.0)
-                                {
-                                    escaped = true;
-                                    break;
-                                }
-                            }
-                            if(escaped) continue;
-                        }
-                        break;
-                }
-                // find the transformed location
-                zx = subx;
-                zy = suby;
-                if(algorithm == Mandelbrot)
-                {
-                    cx = zx;
-                    cy = zy;
-                }
-                switch(interpolation)
-                {
-                    case Linear:
-                    {
-                        double ox=zx,oy=zy; // remember the previous iteration
-                        for(int i=0;i<itarget+1;i++)
-                        {
-                            ox = zx;
-                            oy = zy;
-                            tempx = zx*zx - zy*zy + cx;
-                            zy = 2.0*zx*zy + cy;
-                            zx = tempx;
-                        }
-                        // use linear interpolation on the last jump
-                        zx = ox + (zx-ox)*t;
-                        zy = oy + (zy-oy)*t;
-                    }
-                    break;
-                    case Stirring:
-                    {
-                        // perform the integer stages
-                        for(int i=0;i<itarget;i++)
-                        {
-                            tempx = zx*zx - zy*zy + cx;
-                            zy = 2.0*zx*zy + cy;
-                            zx = tempx;
-                        }
-                        // perform the leftover fractional iterations
-                        ComplexPower(zx,zy,1.0+t);
-                        zx += cx * t;
-                        zy += cy * t;
-                    }
-                    break;
-                }
-                // brighten the new location (if on the image)
-                if(zx>xmin && zx<xmax && zy>ymin && zy<ymax)
-                {
-                    int ix = int(  WIDTH * (zx-xmin) / (xmax-xmin) );
-                    int iy = int( HEIGHT * (zy-ymin) / (ymax-ymin) );
-                    float_image[iy*WIDTH*3+ix*3+0] += 1.0f;
-                    float_image[iy*WIDTH*3+ix*3+1] += 1.0f;
-                    float_image[iy*WIDTH*3+ix*3+2] += 1.0f;
-                }
-            }
-        }
-
-        // composite the float image and the background one
-        for(int y=0;y<HEIGHT;y++)
-        {
-            for(int x=0;x<WIDTH;x++)
-            {
-                float f = float_image[y*WIDTH*3+x*3+0];
-                if(f>0.0f)
-                {
-                    f = log(log(f))*120;
-                    if(f<0) continue;
-                    int ib = int(image[y*WIDTH*4+x*4+0] + f);
-                    image[y*WIDTH*4+x*4+0] = min(255,max(0,ib));
-                    int ig = int(image[y*WIDTH*4+x*4+1] + f);
-                    image[y*WIDTH*4+x*4+1] = min(255,max(0,ig));
-                    int ir = int(image[y*WIDTH*4+x*4+2] + f);
-                    image[y*WIDTH*4+x*4+2] = min(255,max(0,ir));
-                }
-            }
-        }
-
-        // simple TGA output
-        {
-            ostringstream filename;
-            filename << "linear/frame_" << frame << ".tga";
-            ofstream o(filename.str().c_str(), ios::out | ios::binary);
-            for(int c=0;c<12;c++) o.put("002000000000"[c]-'0');
-            o.put((WIDTH & 0x00FF));
-            o.put((WIDTH & 0xFF00) / 256);
-            o.put((HEIGHT & 0x00FF));
-            o.put((HEIGHT & 0xFF00) / 256);
-            o.put(32);
-            o.put(0);
-            o.write((const char*)image,HEIGHT*WIDTH*4);
-        }
+        Point p;
+        p.x = prev_z.x + u * (z.x - prev_z.x);
+        p.y = prev_z.y + u * (z.y - prev_z.y);
+        return p;
     }
+};
 
-    delete []image;
-    
-    return EXIT_SUCCESS;
+struct Rect {
+    double xmin, xmax, ymin, ymax;
+};
+
+void writeTGA(const vector<unsigned char>& char_bgra_image, int width, int height, const string& filename)
+{
+    ofstream o(filename, ios::out | ios::binary);
+    for(int c = 0; c < 12; c++) o.put("002000000000"[c] - '0');
+    o.put((width & 0x00FF));
+    o.put((width & 0xFF00) / 256);
+    o.put((height & 0x00FF));
+    o.put((height & 0xFF00) / 256);
+    o.put(32);
+    o.put(0);
+    o.write(reinterpret_cast<const char*>(char_bgra_image.data()), height * width * 4);
 }
 
-// ---------------------------------------------------------------------------------
+void accumulatePoint(double x, double y, vector<float>& float_rgb_image, const Rect& image_range, int width, int height)
+{
+    const int ix = static_cast<int>( width * (x - image_range.xmin) / (image_range.xmax - image_range.xmin));
+    const int iy = static_cast<int>(height * (y - image_range.ymin) / (image_range.ymax - image_range.ymin));
+    if(ix >= 0 && ix < width && iy >= 0 && iy < height)
+    {
+        const size_t iPixel = iy * width + ix;
+        float_rgb_image[iPixel * 3 + 0] += 1;
+        float_rgb_image[iPixel * 3 + 1] += 1;
+        float_rgb_image[iPixel * 3 + 2] += 1;
+    }
+}
 
-void DrawChar(unsigned char* image,int W,int H,int &sx,char c)
+void writePointsToFloatImage(const vector<MandelbrotPoint>& points, double u, vector<float>& float_rgb_image, const Rect& image_range, int width, int height)
+{
+    fill(float_rgb_image.begin(), float_rgb_image.end(), 0.0f);
+    for(const MandelbrotPoint& pt : points)
+    {
+        const Point p = pt.getIntermediate(u);
+        if(p.x * p.x + p.y * p.y < 4)
+        {
+            // accumulate in both halves since we only track points starting with y > 0
+            accumulatePoint(p.x,  p.y, float_rgb_image, image_range, width, height);
+            accumulatePoint(p.x, -p.y, float_rgb_image, image_range, width, height);
+        }
+    }
+}
+
+unsigned char scaleFloatToChar(float val)
+{
+    if(val > 0)
+    {
+        val = log(log(val)) * 110;
+    }
+    return static_cast<unsigned char>(min(255.0f, max(0.0f, val)));
+}
+
+void writeFloatImageToCharImage(const vector<float>& float_rgb_image, vector<unsigned char>& char_bgra_image, int width, int height)
+{
+    for(int y = 0; y < height; y++)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            const size_t iPixel = y * width + x;
+            const float r = float_rgb_image[iPixel * 3 + 0];
+            const float g = float_rgb_image[iPixel * 3 + 1];
+            const float b = float_rgb_image[iPixel * 3 + 2];
+            char_bgra_image[iPixel * 4 + 0] = scaleFloatToChar(b);
+            char_bgra_image[iPixel * 4 + 1] = scaleFloatToChar(g);
+            char_bgra_image[iPixel * 4 + 2] = scaleFloatToChar(r);
+            char_bgra_image[iPixel * 4 + 3] = 255;
+        }
+    }
+}
+
+void initializeTrackingPoints(vector<MandelbrotPoint>& points, const Rect& sample_range, size_t samples_per_unit_length)
+{
+    const double d = 1 / static_cast<double>(samples_per_unit_length);
+    for(double x = sample_range.xmin; x < sample_range.xmax; x += d)
+    {
+        for(double y = sample_range.ymin; y < sample_range.ymax; y += d)
+        {
+            if(x * x + y * y > 4)
+            {
+                continue; // skip points outside the r=2 disk
+            }
+            MandelbrotPoint pt;
+            pt.c.x = pt.z.x = x;
+            pt.c.y = pt.z.y = y;
+            pt.prev_z.x = pt.prev_z.y = 0;
+            if(points.size() < points.capacity())
+            {
+                points.push_back(pt);
+            }
+        }
+    }
+}
+
+void updatePoints(vector<MandelbrotPoint>& points)
+{
+    for(MandelbrotPoint& pt : points)
+    {
+        pt.update();
+    }
+}
+
+string getFilename(size_t iFrame)
+{
+    ostringstream oss;
+    oss << "frame_" << setw(6) << setfill('0') << iFrame << ".tga";
+    return oss.str();
+}
+
+void drawCharacterOnCharImage(vector<unsigned char>& image, int W, int H, int &sx, char c)
 {
     const char *segments[11] = {"012456","25","02346","02356","1235","01356","013456","025","0123456","012356","7"}; // 0123456789.
     const int coords[8][4] = {{1,0,3,1},{0,1,1,4},{3,1,4,4},{1,4,3,5},{0,5,1,8},{3,5,4,8},{1,8,3,9},{1,4,2,5}}; // {xmin,ymin,xmax,ymax}
     const int scale = 3;
-    const int sy = scale*9*2;
+    const int sy = H - scale*9*2;
 
     const char *segs;
     if(c>='0' && c<='9') segs = segments[c-'0'];
@@ -312,15 +181,80 @@ void DrawChar(unsigned char* image,int W,int H,int &sx,char c)
     sx += scale*6;
 }
 
-// ---------------------------------------------------------------------------------
-
-void DrawNumber(unsigned char* image,int W,int H,double f)
+void drawNumberOnCharImage(vector<unsigned char>& image, int width, int height, double f)
 {
     ostringstream oss;
     oss << f;
-    int sx=0;
-    for(int iC=0;iC<oss.str().length();iC++)
-        DrawChar(image,W,H,sx,oss.str()[iC]);
+    int sx = width / 20;
+    for(int iC = 0; iC < oss.str().length(); iC++)
+    {
+        drawCharacterOnCharImage(image, width, height, sx, oss.str()[iC]);
+    }
 }
 
-// ---------------------------------------------------------------------------------
+void run()
+{
+    // define the region that we sample with tracking points (probably don't want to change this)
+    constexpr Rect sample_range = { -2, 2, 0, 2 }; // (we duplicate the upper half to save compute)
+
+    // sample the region at as high a resolution as we can afford
+    constexpr size_t samples_per_unit_length = 8000; // samples per unit length on the complex plane
+    constexpr size_t MAX_POINTS = static_cast<size_t>(samples_per_unit_length * samples_per_unit_length
+                                            * (sample_range.ymax - sample_range.ymin) * (sample_range.xmax - sample_range.xmin));
+    vector<MandelbrotPoint> points;
+    points.reserve(MAX_POINTS);
+
+    // define the region that we want to see in the final image (can change this freely)
+    constexpr Rect image_range = { -1.8, 0.9, -0.8, 1.2};
+
+    // make a destination image to write into
+    constexpr int HEIGHT = 1024;
+    constexpr int WIDTH = static_cast<int>(HEIGHT * (image_range.xmax - image_range.xmin) / (image_range.ymax - image_range.ymin));
+    cout << "Writing into " << WIDTH << " x " << HEIGHT << " image\n";
+    vector<float> float_rgb_image(HEIGHT*WIDTH*3); // row,column RGB - we accumulate point counts into this one
+    vector<unsigned char> char_bgra_image(HEIGHT*WIDTH*4); // row,column BGRA - this contains our final image
+
+    initializeTrackingPoints(points, sample_range, samples_per_unit_length);
+
+    const size_t gigabytes = MAX_POINTS * sizeof(MandelbrotPoint) / (1024 * 1024 * 1024);
+    const double million_pts = points.size() / 1e6;
+    cout << "Tracking " << million_pts << " million points. Memory: " << gigabytes << "GB" << std::endl;
+
+    const int n_iterations = 100;
+    const int n_sub_iterations = 20;
+    size_t iFrame = 0;
+    for(size_t iIteration = 0; iIteration <= n_iterations; iIteration++)
+    {
+        updatePoints(points);
+        for(size_t iSubIteration = 0; iSubIteration < n_sub_iterations; iSubIteration++)
+        {
+            const double u = iSubIteration / static_cast<double>(n_sub_iterations);
+            writePointsToFloatImage(points, u, float_rgb_image, image_range, WIDTH, HEIGHT);
+            writeFloatImageToCharImage(float_rgb_image, char_bgra_image, WIDTH, HEIGHT);
+            drawNumberOnCharImage(char_bgra_image, WIDTH, HEIGHT, iIteration + u);
+            const string filename = getFilename(iFrame++);
+            writeTGA(char_bgra_image, WIDTH, HEIGHT, filename);
+            cout << "Iteration " << iIteration + u << " - wrote " << filename << "\n";
+        }
+    }
+}
+
+int main()
+{
+    try
+    {
+        run();
+    }
+    catch(const exception& e)
+    {
+        cout << "Caught an exception: " << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+    catch(...)
+    {
+        cout << "Caught an unknown exception\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
