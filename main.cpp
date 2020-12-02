de8225b72c8c5dd8011b14a65d1c04105975ddbe
Tim Hutton <tihutt@microsoft.com>
2020-12-02 21:49:18 +0000

    Using floats to save memory. Made code more flexible.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@
     along with MandelStir. If not, see <http://www.gnu.org/licenses/>.         */
 
 // stdlib
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <iomanip>
@@ -23,43 +25,60 @@
 #include <sstream>
 #include <vector>
 
-using namespace std;
+using FloatType = float;
 
 struct Point {
-    double x, y;
-    Point(double x, double y) : x(x), y(y) {}
+    FloatType x, y;
+    Point(FloatType x, FloatType y) : x(x), y(y) {}
 };
 
-bool inR2Disk(double x, double y)
+bool inR2Disk(FloatType x, FloatType y)
 {
     return x * x + y * y < 4;
 }
 
-bool inBulb1(double x, double y)
+bool inBulb1(FloatType x, FloatType y)
 {
-    const double cx = -1;
-    const double cy = 0;
-    const double r = 0.25;
+    const FloatType cx = -1;
+    const FloatType cy = 0;
+    const FloatType r = 0.25f;
     return (x - cx) * (x - cx) + (y - cy) * (y - cy) < r * r;
 }
 
-bool inBulb2(double x, double y)
+bool inBulb2(FloatType x, FloatType y)
 {
     // this is only approximate
-    const double cx = -0.1253;
-    const double cy = 0.745;
-    const double r = 0.094;
+    const FloatType cx = -0.1253f;
+    const FloatType cy = 0.745f;
+    const FloatType r = 0.094f;
     return (x - cx) * (x - cx) + (y - cy) * (y - cy) < r * r;
 }
 
-bool inCardioid(double x, double y)
+bool inCardioid(FloatType x, FloatType y)
 {
-    const double tx = 0.25 - x;
-    const double ty = y;
-    const double theta = atan2(ty, tx);
-    const double cr = pow(cos(theta / 2), 2);
-    const double r2 = tx * tx + ty * ty;
+    const FloatType tx = 0.25f - x;
+    const FloatType ty = y;
+    const FloatType theta = atan2(ty, tx);
+    const FloatType cr = pow(cos(theta / 2.0f), 2.0f);
+    const FloatType r2 = tx * tx + ty * ty;
     return r2 < cr * cr;
+}
+
+bool inSet(FloatType x, FloatType y, size_t n_iterations = 1000)
+{
+    const FloatType cx = x;
+    const FloatType cy = y;
+    for(size_t i = 0; i < n_iterations; i++)
+    {
+        FloatType tempx = x * x - y * y + cx;
+        y = 2 * x * y + cy;
+        x = tempx;
+        if(!inR2Disk(x,y))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 struct MandelbrotPoint {
@@ -68,12 +87,11 @@ struct MandelbrotPoint {
     Point prev_z;
     bool is_in_set;
 
-    MandelbrotPoint(double x, double y)
+    MandelbrotPoint(FloatType x, FloatType y)
         : c(x, y)
         , z(x, y)
         , prev_z(0, 0)
     {
-        is_in_set = isInSet();
     }
 
     void update()
@@ -84,38 +102,19 @@ struct MandelbrotPoint {
         z.y = 2 * prev_z.x * prev_z.y + c.y;
     }
 
-    Point getIntermediate(double u) const
+    Point getIntermediate(FloatType u) const
     {
         return Point(prev_z.x + u * (z.x - prev_z.x), prev_z.y + u * (z.y - prev_z.y));
-    }
-
-private:
-
-    bool isInSet(size_t n_iterations = 100) const
-    {
-        double x = c.x;
-        double y = c.y;
-        for(size_t i = 0; i < n_iterations; i++)
-        {
-            double tempx = x * x - y * y + c.x;
-            y = 2 * x * y + c.y;
-            x = tempx;
-            if(!inR2Disk(x,y))
-            {
-                return false;
-            }
-        }
-        return true;
     }
 };
 
 struct Rect {
-    double xmin, xmax, ymin, ymax;
+    FloatType xmin, xmax, ymin, ymax;
 };
 
-void writeTGA(const vector<unsigned char>& char_bgra_image, int width, int height, const string& filename)
+void writeTGA(const std::vector<unsigned char>& char_bgra_image, int width, int height, const std::string& filename)
 {
-    ofstream o(filename, ios::out | ios::binary);
+    std::ofstream o(filename, std::ios::out | std::ios::binary);
     for(int c = 0; c < 12; c++) o.put("002000000000"[c] - '0');
     o.put((width & 0x00FF));
     o.put((width & 0xFF00) / 256);
@@ -126,7 +125,7 @@ void writeTGA(const vector<unsigned char>& char_bgra_image, int width, int heigh
     o.write(reinterpret_cast<const char*>(char_bgra_image.data()), height * width * 4);
 }
 
-void accumulatePoint(double x, double y, bool is_in_set, vector<float>& float_rgb_image, const Rect& image_range, int width, int height)
+void accumulatePoint(FloatType x, FloatType y, bool is_in_set, std::vector<float>& float_rgb_image, const Rect& image_range, int width, int height)
 {
     const int ix = static_cast<int>( width * (x - image_range.xmin) / (image_range.xmax - image_range.xmin));
     const int iy = static_cast<int>(height * (y - image_range.ymin) / (image_range.ymax - image_range.ymin));
@@ -138,15 +137,15 @@ void accumulatePoint(double x, double y, bool is_in_set, vector<float>& float_rg
     }
 }
 
-void writePointsToFloatImage(const vector<MandelbrotPoint>& points,
-                             double u,
-                             vector<float>& float_rgb_image,
+void writePointsToFloatImage(const std::vector<MandelbrotPoint>& points,
+                             FloatType u,
+                             std::vector<float>& float_rgb_image,
                              const Rect& image_range,
                              int width,
                              int height,
                              bool mirror)
 {
-    fill(float_rgb_image.begin(), float_rgb_image.end(), 0.0f);
+    std::fill(float_rgb_image.begin(), float_rgb_image.end(), 0.0f);
     for(const MandelbrotPoint& pt : points)
     {
         const Point p = pt.getIntermediate(u);
@@ -168,10 +167,10 @@ unsigned char scaleFloatToChar(float val)
     {
         val = log(log(val)) * 110;
     }
-    return static_cast<unsigned char>(min(255.0f, max(0.0f, val)));
+    return static_cast<unsigned char>(std::min(255.0f, std::max(0.0f, val)));
 }
 
-void writeFloatImageToCharImage(const vector<float>& float_rgb_image, vector<unsigned char>& char_bgra_image, int width, int height)
+void writeFloatImageToCharImage(const std::vector<float>& float_rgb_image, std::vector<unsigned char>& char_bgra_image, int width, int height)
 {
     for(int y = 0; y < height; y++)
     {
@@ -186,38 +185,43 @@ void writeFloatImageToCharImage(const vector<float>& float_rgb_image, vector<uns
     }
 }
 
-void initializeTrackingPoints(vector<MandelbrotPoint>& points, const Rect& sample_range, size_t samples_per_unit_length)
+template<typename Func>
+void initializeTrackingPoints(std::vector<MandelbrotPoint>& points, const Rect& sample_range, size_t samples_per_unit_length, Func inclusion_proc)
 {
-    const double d = 1 / static_cast<double>(samples_per_unit_length);
-    for(double x = sample_range.xmin; x < sample_range.xmax; x += d)
+    const FloatType d = 1.0f / samples_per_unit_length;
+    FloatType next_target = sample_range.ymin + (sample_range.ymax - sample_range.ymin)/10;
+    for(FloatType y = sample_range.ymin; y < sample_range.ymax; y += d)
     {
-        for(double y = sample_range.ymin; y < sample_range.ymax; y += d)
+        if(y > next_target)
         {
-            if(inBulb2(x, y))
-            {
-                MandelbrotPoint pt(x, y);
-                points.push_back(pt);
-            }
+            const int percentage = static_cast<int>(100 * (y - sample_range.ymin) / (sample_range.ymax - sample_range.ymin));
+            std::cout << percentage << "% ";
+            next_target += (sample_range.ymax - sample_range.ymin)/10;
+        }
+        for(FloatType x = sample_range.xmin; x < sample_range.xmax; x += d)
+        {
+            inclusion_proc(x, y, points);
         }
     }
 }
 
-void updatePoints(vector<MandelbrotPoint>& points)
+void updatePoints(std::vector<MandelbrotPoint>& points)
 {
-    for(MandelbrotPoint& pt : points)
-    {
+    // update the position of each tracking point but also remove them if they have moved off the disk
+    points.erase(std::remove_if(points.begin(), points.end(), [](MandelbrotPoint& pt) -> bool {
         pt.update();
-    }
+        return !inR2Disk(pt.prev_z.x, pt.prev_z.y);
+    }), points.end());
 }
 
-string getFilename(size_t iFrame)
+std::string getFilename(size_t iFrame)
 {
-    ostringstream oss;
-    oss << "frame_" << setw(6) << setfill('0') << iFrame << ".tga";
+    std::ostringstream oss;
+    oss << "frame_" << std::setw(6) << std::setfill('0') << iFrame << ".tga";
     return oss.str();
 }
 
-void drawCharacterOnCharImage(vector<unsigned char>& image, int W, int H, int &sx, char c)
+void drawCharacterOnCharImage(std::vector<unsigned char>& image, int W, int H, int &sx, char c)
 {
     const char *segments[11] = {"012456","25","02346","02356","1235","01356","013456","025","0123456","012356","7"}; // 0123456789.
     const int coords[8][4] = {{1,0,3,1},{0,1,1,4},{3,1,4,4},{1,4,3,5},{0,5,1,8},{3,5,4,8},{1,8,3,9},{1,4,2,5}}; // {xmin,ymin,xmax,ymax}
@@ -237,9 +241,9 @@ void drawCharacterOnCharImage(vector<unsigned char>& image, int W, int H, int &s
     sx += scale*6;
 }
 
-void drawNumberOnCharImage(vector<unsigned char>& image, int width, int height, double f)
+void drawNumberOnCharImage(std::vector<unsigned char>& image, int width, int height, FloatType f)
 {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << f;
     int sx = width / 20;
     for(int iC = 0; iC < oss.str().length(); iC++)
@@ -248,7 +252,7 @@ void drawNumberOnCharImage(vector<unsigned char>& image, int width, int height, 
     }
 }
 
-void writeBackgroundImage(vector<unsigned char>& char_bgra_background_image, int width, int height, const Rect& image_range)
+void writeBackgroundImage(std::vector<unsigned char>& char_bgra_background_image, int width, int height, const Rect& image_range)
 {
     for(int y = 0; y < height; y++)
     {
@@ -256,6 +260,7 @@ void writeBackgroundImage(vector<unsigned char>& char_bgra_background_image, int
         {
             MandelbrotPoint pt(image_range.xmin + x * (image_range.xmax - image_range.xmin) / width,
                                image_range.ymin + y * (image_range.ymax - image_range.ymin) / height);
+            pt.is_in_set = inSet(pt.c.x, pt.c.y);
             const size_t iPixel = y * width + x;
             char_bgra_background_image[iPixel * 4 + 0] = pt.is_in_set ? 150 : 0; // Mandelbrot set in blue pixel
             char_bgra_background_image[iPixel * 4 + 1] = 0;
@@ -265,7 +270,7 @@ void writeBackgroundImage(vector<unsigned char>& char_bgra_background_image, int
     }
 }
 
-void copyBackgroundImageToCharImage(const vector<unsigned char>& char_bgra_background_image, vector<unsigned char>& char_bgra_image)
+void copyBackgroundImageToCharImage(const std::vector<unsigned char>& char_bgra_background_image, std::vector<unsigned char>& char_bgra_image)
 {
     char_bgra_image.assign(char_bgra_background_image.begin(), char_bgra_background_image.end());
 }
@@ -274,52 +279,65 @@ void run()
 {
     // define the region that we sample with tracking points (probably don't want to change this)
     constexpr Rect sample_range = { -2, 2, 0, 2 }; // (we duplicate the upper half to save compute)
-    constexpr bool mirror = false;
+    constexpr bool mirror = true;
 
     // sample the region at as high a resolution as we can afford
     constexpr size_t samples_per_unit_length = 8000; // samples per unit length on the complex plane
+    const FloatType max_expected_bytes = 2.0f * static_cast<FloatType>(M_PI) * samples_per_unit_length * samples_per_unit_length * sizeof(MandelbrotPoint);
+    const FloatType max_expected_gigabytes = max_expected_bytes / (1024 * 1024 * 1024);
+    // (can expect 2*pi*samples_per_unit_length points, each stored in
     constexpr size_t MAX_POINTS = static_cast<size_t>(samples_per_unit_length * samples_per_unit_length
                                             * (sample_range.ymax - sample_range.ymin) * (sample_range.xmax - sample_range.xmin));
-    const size_t gigabytes = MAX_POINTS * sizeof(MandelbrotPoint) / (1024 * 1024 * 1024);
-    vector<MandelbrotPoint> points;
-    points.reserve(MAX_POINTS);
+    std::vector<MandelbrotPoint> points;
 
     // define the region that we want to see in the final image (can change this freely)
-    constexpr Rect image_range = { -1.4, 0.7, -0.5, 1.1};
+    constexpr Rect image_range = { -2, 1.8f, -1.2f, 1.2f};
 
     // make a destination image to write into
     constexpr int HEIGHT = 1024;
     constexpr int WIDTH = static_cast<int>(HEIGHT * (image_range.xmax - image_range.xmin) / (image_range.ymax - image_range.ymin));
-    vector<float> float_rgb_image(HEIGHT*WIDTH*3); // row,column RGB - we accumulate point counts into this one
-    vector<unsigned char> char_bgra_background_image(HEIGHT*WIDTH*4); // row,column BGRA - this contains a static background image
-    vector<unsigned char> char_bgra_image(HEIGHT*WIDTH*4); // row,column BGRA - this contains our final image
+    std::vector<float> float_rgb_image(HEIGHT*WIDTH*3); // row,column RGB - we accumulate point counts into this one
+    std::vector<unsigned char> char_bgra_background_image(HEIGHT*WIDTH*4); // row,column BGRA - this contains a static background image
+    std::vector<unsigned char> char_bgra_image(HEIGHT*WIDTH*4); // row,column BGRA - this contains our final image
 
-    cout << "Writing the background image..." << endl;
+    std::cout << "Writing the background image (" << WIDTH << " x " << HEIGHT << ")..." << std::endl;
     writeBackgroundImage(char_bgra_background_image, WIDTH, HEIGHT, image_range);
-    cout << "Initializing the tracking points..." << endl;
-    initializeTrackingPoints(points, sample_range, samples_per_unit_length);
+    std::cout << "Initializing the tracking points... (Up to " << std::fixed << std::setprecision(1) << max_expected_gigabytes << " GB to be allocated.)" << std::endl;
+    initializeTrackingPoints(points, sample_range, samples_per_unit_length, [](FloatType x, FloatType y, std::vector<MandelbrotPoint>& points) {
+        if(inCardioid(x, y))
+        {
+            MandelbrotPoint pt(x, y);
+            pt.is_in_set = true;
+            points.push_back(pt);
+        }
+    });
 
-    const double million_pts = points.size() / 1e6;
-    cout << "Tracking " << million_pts << " million points. (" << gigabytes << " GB)" << endl;
-    cout << "Writing into " << WIDTH << " x " << HEIGHT << " image\n";
+    const FloatType million_pts = points.size() / 1e6f;
+    const size_t gigabytes = sizeof(MandelbrotPoint) * points.size() / (1024 * 1024 * 1024);
+    std::cout << "Tracking " << million_pts << " million points. (Memory usage: " << gigabytes << " GB)" << std::endl;
 
     const int n_iterations = 1000;
     int iFrame = 0;
     for(int iIteration = 0; iIteration <= n_iterations; iIteration++)
     {
-        const int n_sub_iterations = max(1, 50 - iIteration);
+        const int n_sub_iterations = std::max(5, 50 - iIteration); // speed up as we go along
+        std::cout << "Starting iteration " << iIteration << " (" << points.size() << " points remaining)" << std::endl;
         updatePoints(points);
         for(int iSubIteration = 0; iSubIteration < n_sub_iterations; iSubIteration++)
         {
-            const double u = iSubIteration / static_cast<double>(n_sub_iterations);
-            cout << "Iteration " << iIteration + u << " ";
+            const FloatType u = iSubIteration / static_cast<FloatType>(n_sub_iterations);
+            std::cout << "Computing frame at " << std::fixed << std::setprecision(5) << iIteration + u;
             writePointsToFloatImage(points, u, float_rgb_image, image_range, WIDTH, HEIGHT, mirror);
+            std::cout << ".";
             copyBackgroundImageToCharImage(char_bgra_background_image, char_bgra_image);
+            std::cout << ".";
             writeFloatImageToCharImage(float_rgb_image, char_bgra_image, WIDTH, HEIGHT);
+            std::cout << ".";
             drawNumberOnCharImage(char_bgra_image, WIDTH, HEIGHT, iIteration + u);
-            const string filename = getFilename(iFrame++);
+            std::cout << ".";
+            const std::string filename = getFilename(iFrame++);
             writeTGA(char_bgra_image, WIDTH, HEIGHT, filename);
-            cout << "Wrote " << filename << endl;
+            std::cout << " Wrote " << filename << std::endl;
         }
     }
 }
@@ -330,14 +348,14 @@ int main()
     {
         run();
     }
-    catch(const exception& e)
+    catch(const std::exception& e)
     {
-        cout << "Caught an exception: " << e.what() << endl;
+        std::cout << "Caught an exception: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
     catch(...)
     {
-        cout << "Caught an unknown exception" << endl;
+        std::cout << "Caught an unknown exception" << std::endl;
         return EXIT_FAILURE;
     }
 
